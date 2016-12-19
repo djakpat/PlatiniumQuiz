e3a7489bdb8c777bcff5c4f581b5f8f9eba45e63
GGI59 <gg_2@hotmail.fr>
2016-12-19 10:53:51 +0100

    Add question view.

--- app/src/main/java/com/jack_baretto/platiniumquiz/MainActivity.java
+++ app/src/main/java/com/jack_baretto/platiniumquiz/MainActivity.java
@@ -3,6 +3,7 @@ package com.jack_baretto.platiniumquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +13,12 @@ public class MainActivity extends AppCompatActivity {
     /*
      * View for the answer choices.
      */
-    ListView choicesView;
+    private ListView choicesView;
+
+    /**
+     * View for the MCQ question.
+     */
+    private TextView questionView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,16 @@ public class MainActivity extends AppCompatActivity {
         List<Choice> choices = generateChoices();
         ChoiceAdapter adapter = new ChoiceAdapter(MainActivity.this, choices);
         choicesView.setAdapter(adapter);
+        questionView = (TextView) findViewById(R.id.question);
+        questionView.setText(this.loadQuestion());
+    }
+
+    /**
+     * Load the MCQ question to print.
+     * @return The question to print.
+     */
+    private String loadQuestion(){
+        return "Any question ?";
     }
 
     /**
