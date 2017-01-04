479c83b7aa030e422363d910e4dc231394132a5c
GGI59 <gg_2@hotmail.fr>
2017-01-04 17:14:55 +0100

    Add continue button. Mock data and result button.

--- app/src/main/java/com/jack_baretto/platiniumquiz/MainActivity.java
+++ app/src/main/java/com/jack_baretto/platiniumquiz/MainActivity.java
@@ -25,45 +25,71 @@ public class MainActivity extends AppCompatActivity {
     /** Button to go to the next question. */
     private Button nextButton;
 
-    /** Compteur. */
-    int compteur = 1;
+    /** Button to view the result. */
+    private Button resultButton;
+
+    /** Questions of the MCQ. **/
+    List<QuestionModel> questions = new ArrayList<>();
+
+    // Index of the current page of the MCQ.
+    private int currentPageIndex = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final QuestionModel data = generateQuestionModel("Any Question ?");
+        questions = this.getMCQQuestions();
         questionView = (TextView) findViewById(R.id.question);
-        questionView.setText(data.getQuestion());
+        questionView.setText(questions.get(currentPageIndex).getQuestion());
         choicesView = (ListView) findViewById(R.id.choicesView);
-        final ChoiceAdapter adapter = new ChoiceAdapter(MainActivity.this, data.getChoices());
+        final ChoiceAdapter adapter = new ChoiceAdapter(MainActivity.this, questions.get(0).getChoices());
         choicesView.setAdapter(adapter);
+        resultButton = (Button) findViewById(R.id.result);
         nextButton = (Button) findViewById(R.id.next);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                QuestionModel newQuestion = generateQuestionModel("AnotherQuestion");
-                questionView.setText(newQuestion.getQuestion() + compteur);
+                currentPageIndex ++;
+                questionView.setText(questions.get(currentPageIndex).getQuestion());
                 adapter.clear();
-                adapter.addAll(newQuestion.getChoices());
+                adapter.addAll(questions.get(currentPageIndex).getChoices());
                 adapter.notifyDataSetChanged();
-                compteur++;
+                if (isLastPage()) {
+                    nextButton.setEnabled(false);
+                    resultButton.setVisibility(View.VISIBLE);
+                }
             }
         });
     }
+
+    private boolean isLastPage() {
+        return currentPageIndex < questions.size();
+    }
+
 
     /**
      * Generate a {@link QuestionModel}for a question.
      * @return a {@link QuestionModel}.
      */
-    private QuestionModel generateQuestionModel(String question) {
+    private QuestionModel generateQuestionModel(int i) {
         QuestionModel questionModel = new QuestionModel();
-        questionModel.setQuestion(question);
+        questionModel.setQuestion("Question " + i + " :");
         List<Choice> choices = new ArrayList<>();
-        for(int i = 1; i <= 20; i++){
-            choices.add(new Choice("Réponse " + compteur + " " + i));
-        }
+        choices.add(new Choice("Réponse " + i + ".1"));
+        choices.add(new Choice("Réponse " + i + ".2"));
         questionModel.setChoices(choices);
         return questionModel;
+    }
+
+    /**
+     * Get all the questions for the MCQ.
+     * @return The {@link QuestionModel} list.
+     */
+    private List<QuestionModel> getMCQQuestions(){
+        List<QuestionModel> questions = new ArrayList<>();
+        questions.add(generateQuestionModel(1));
+        questions.add(generateQuestionModel(2));
+        return questions;
     }
 }
