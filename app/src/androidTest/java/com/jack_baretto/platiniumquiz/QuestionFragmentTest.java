a36610860d13d90770ee7b824be81651c8d3f550
GGI59 <gg_2@hotmail.fr>
2017-01-12 11:01:30 +0100

    Add test for the question fragment.

---
+++ app/src/androidTest/java/com/jack_baretto/platiniumquiz/QuestionFragmentTest.java
@@ -0,0 +1,36 @@
+package com.jack_baretto.platiniumquiz;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+
+/**
+ *  Test of {@link QuestionFragment}.
+ * Created by GGI on 12/01/2017.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class QuestionFragmentTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    /**
+     * Check if the button to go to the next question is available.
+     */
+    @Test
+    public void hasNextButton(){
+        Espresso.onView(ViewMatchers.withId(R.id.next)).check(ViewAssertions.matches(isEnabled()));
+    }
+}
