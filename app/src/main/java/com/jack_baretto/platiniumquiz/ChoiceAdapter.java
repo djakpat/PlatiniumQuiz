603ce8ce45484b1de1d805362ac3dad2bc79db2b
GGI59 <gg_2@hotmail.fr>
2017-01-19 12:01:32 +0100

    Add the possibility to choice between signle selection mode or multi selection.

--- app/src/main/java/com/jack_baretto/platiniumquiz/ChoiceAdapter.java
+++ app/src/main/java/com/jack_baretto/platiniumquiz/ChoiceAdapter.java
@@ -1,54 +1,92 @@
 package com.jack_baretto.platiniumquiz;
 
-import android.content.Context;
+import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
-import android.widget.TextView;
 
 import com.baretto.mcq.datamodel.Choice;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ChoiceAdapter extends ArrayAdapter<Choice> {
 
-    public ChoiceAdapter(Context context, List<Choice> choices) {
-        super(context, 0, choices);
+    private List<Choice> choices;
+    private Activity activity;
+    private int selectedPosition = -1;
+    private boolean isSingleChoice;
+
+    public ChoiceAdapter(Activity context, int resource, List<Choice> objects, boolean isSingleChoice) {
+        super(context, resource, objects);
+        this.activity = context;
+        this.choices = objects;
+        this.isSingleChoice = isSingleChoice;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if(convertView == null){
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.choice, parent, false);
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+        // If holder not exist then locate all view from UI file.
+        if (convertView == null) {
+            // inflate UI from XML file
+            convertView = inflater.inflate(R.layout.choice, parent, false);
+            // get all UI view
+            holder = new ViewHolder(convertView);
+            // set tag for holder
+            convertView.setTag(holder);
+        } else {
+            // if holder created, get tag from view
+            holder = (ViewHolder) convertView.getTag();
         }
-        PropositionViewHolder viewHolder = (PropositionViewHolder) convertView.getTag();
-        if(viewHolder == null){
-            viewHolder = new PropositionViewHolder();
-            viewHolder.checkBox = (CheckBox) convertView.findViewById(R.id.checkBox);
-            convertView.setTag(viewHolder);
+        holder.checkBox.setTag(position); // This line is important.
+        holder.checkBox.setText(getItem(position).getLabel());
+        Choice currentChoice = (Choice) choices.get(position);
+        // Checked if the choice is already selected
+        holder.checkBox.setChecked(currentChoice.isSelected());
+        if(this.isSingleChoice){
+            holder.checkBox.setOnClickListener(onStateChangedSingleChoiceListener(holder.checkBox, position));
+        } else {
+            holder.checkBox.setOnClickListener(onStateChangedMultipleChoiceListener(holder.checkBox, position));
         }
-        //getItem(position) va récupérer l'item [position] de la liste
-        Choice choice = getItem(position);
-        //il ne reste plus qu'à remplir notre vue
-        viewHolder.checkBox.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CheckBox checkBox = (CheckBox) v;
-                Choice currentChoice = (Choice) checkBox.getTag();
-                currentChoice.setSelected(((CheckBox) v).isChecked());
-            }
-        });
-        viewHolder.checkBox.setTag(choice);
-        viewHolder.checkBox.setText(choice.getLabel());
-        viewHolder.checkBox.setChecked(choice.isSelected());
         return convertView;
     }
 
+    private View.OnClickListener onStateChangedMultipleChoiceListener(final CheckBox checkBox, final int position) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choices.get(position).setSelected(checkBox.isChecked());
+                notifyDataSetChanged();
+            }
+        };
+    }
 
-    private class PropositionViewHolder{
-        public CheckBox checkBox;
+    private View.OnClickListener onStateChangedSingleChoiceListener(final CheckBox checkBox, final int position) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBox.isChecked()) {
+                    choices.get(position).setSelected(true);
+                }
+                for(Choice choice : choices){
+                    if(!choice.equals(choices.get(position))){
+                        choice.setSelected(false);
+                    }
+                }
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    private static class ViewHolder {
+        private CheckBox checkBox;
+
+        public ViewHolder(View v) {
+            checkBox = (CheckBox) v.findViewById(R.id.checkBox);
+        }
     }
 }
