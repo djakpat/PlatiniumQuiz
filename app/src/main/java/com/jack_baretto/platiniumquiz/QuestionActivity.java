2ad8ae0f17aec7818ae0152dc6068b16646fc58a
djakpat <djakce2@hotmail.fr>
2017-01-19 13:35:06 +0100

    use MCQ into Fragment

--- app/src/main/java/com/jack_baretto/platiniumquiz/QuestionActivity.java
+++ app/src/main/java/com/jack_baretto/platiniumquiz/QuestionActivity.java
@@ -1,28 +1,29 @@
 package com.jack_baretto.platiniumquiz;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.baretto.mcq.datamodel.Question;
 import com.baretto.mcq.datamodel.internals.MCQ;
 
-import java.io.Serializable;
-
 public class QuestionActivity extends AppCompatActivity {
+
+    MCQ mcq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_activity_layout);
-        MCQ mcq = (MCQ) getIntent().getSerializableExtra("Mcq");
+        mcq = (MCQ) getIntent().getSerializableExtra("Mcq");
         }
 
     public void sendMessage(View view){
         Intent intent = new Intent(this, ResultActivity.class);
         startActivity(intent);
+    }
 
+    public MCQ getMcq() {
+        return mcq;
     }
 }
