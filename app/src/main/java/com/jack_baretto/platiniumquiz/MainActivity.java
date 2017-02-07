f4a6dd60980b62d8ed8c25c3798bc17f72812b2e
djakpat <djakce2@hotmail.fr>
2017-02-07 14:39:11 +0100

    add slider and view result

--- app/src/main/java/com/jack_baretto/platiniumquiz/MainActivity.java
+++ app/src/main/java/com/jack_baretto/platiniumquiz/MainActivity.java
@@ -4,20 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.SeekBar;
 
-import com.baretto.mcq.datamodel.internals.MCQ;
+import com.baretto.mcq.datamodel.MCQ;
+
 
 public class MainActivity extends AppCompatActivity {
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+
     }
 
     public void sendMessage(View view){
 
-        MCQ mcq = new MCQ(3);
+
+        int progress = seekBar.getProgress();
+        MCQ mcq = new MCQ(progress);
 
         Intent intent = new Intent(this, QuestionActivity.class);
         intent.putExtra("Mcq",mcq);
--- app/src/main/java/com/jack_baretto/platiniumquiz/ResultActivity.java
+++ app/src/main/java/com/jack_baretto/platiniumquiz/ResultActivity.java
@@ -2,10 +2,14 @@ package com.jack_baretto.platiniumquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 
+import com.baretto.mcq.datamodel.MCQ;
 import com.baretto.mcq.datamodel.Question;
-import com.baretto.mcq.datamodel.internals.MCQ;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -16,19 +20,25 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         mcq = (MCQ) getIntent().getSerializableExtra("Mcq");
-
+        List<Question> failedQuestions = new ArrayList<Question>();
         int goodResponse = 0;
         for (Question question : mcq.getQuestions()) {
             if (question.answerIsCorrect()) {
                 goodResponse++;
+            } else {
+                failedQuestions.add(question);
             }
         }
 
         TextView result = (TextView) findViewById(R.id.textView);
         result.setText("vous avez : " + goodResponse + " Bonnes Réponses");
+
+
+        ExpandableListView expandableListView = (ExpandableListView) findViewById(R.id.resultQuestionView);
+        QuestionResultAdaptater questionResultAdaptater = new QuestionResultAdaptater(this, failedQuestions);
+
+        expandableListView.setAdapter(questionResultAdaptater);
     }
-
-
 
 
 }
